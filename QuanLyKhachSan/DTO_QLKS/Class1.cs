bfd4ff5f61e9dac3e0d0fc3c21f003aeb2dfc7cc
nmhieu1301 <nmhieu1301@users.noreply.github.com>
2017-12-09 11:26:54 +0700

    Minh Hiếu
    project QLKS

---
+++ QuanLyKhachSan/DTO_QLKS/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTO_QLKS
+{
+    public class Class1
+    {
+    }
+}
